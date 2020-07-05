bfcce2fa1f3b7eb405fa9c197b80b2ea9a849a7b
Karolina Jurczak <inka_c@tlen.pl>
2020-07-05 17:53:22 +0200

    Add first form listener and add http-server

--- client/app.js
+++ client/app.js
@@ -8,3 +8,18 @@ const messageContentInput = document.getElementById('message-content');
 
 // Global variables
 let userName;
+
+// Forms
+function login(event) {
+  event.preventDefault();
+  if(userNameInput.value == '' || userNameInput.value == undefined){
+    alert('Please, type Your name')
+  } else {
+    userName = userNameInput.value; //im not sure is this right place
+    loginForm.classList.remove('show');
+    messagesSection.classList.add('show');
+  } 
+};
+
+loginForm.addEventListener('submit', login);
+
