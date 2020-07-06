0edc8b9aa166f77db777ad97e9a52711a59706bb
Karolina Jurczak <inka_c@tlen.pl>
2020-07-06 10:18:03 +0200

    Add listener for message button

---
+++ README.md
@@ -0,0 +1,8 @@
+### Description:
+In this application I use WebSocket to create simple chat
+
+### Page:
+http://localhost:8080
+
+### How to use in console:
+http-server
--- client/app.js
+++ client/app.js
@@ -1,7 +1,7 @@
 // Reference
 const loginForm = document.getElementById('welcome-form');
 const messagesSection = document.getElementById('messages-section');
-const messagesList = document.getElementById('messages-section__list');
+const messagesList = document.getElementById('messages-list');
 const addMessageForm = document.getElementById('add-messages-form');
 const userNameInput = document.getElementById('username');
 const messageContentInput = document.getElementById('message-content');
@@ -15,11 +15,38 @@ function login(event) {
   if(userNameInput.value == '' || userNameInput.value == undefined){
     alert('Please, type Your name')
   } else {
-    userName = userNameInput.value; //im not sure is this right place
+    userName = userNameInput.value; //im not sure is this right place (inside function?)
     loginForm.classList.remove('show');
     messagesSection.classList.add('show');
-  } 
+  };
+};
+
+function addMessage(author, content) {
+  const message = document.createElement('li');
+  message.classList.add('message');
+  message.classList.add('message--received');
+  if(author == userName){
+    message.classList.add('message--self');
+  };
+  message.innerHTML = 
+    `<h3 class="message__author">
+      ${author == userName ? 'You' : author}
+    </h3>
+    <div class="message__content">
+      ${content}
+    </div>`;
+  messagesList.appendChild(message);
+};
+
+function sendMessage(event) {
+  event.preventDefault();
+  if(messageContentInput.value == '' || messageContentInput.value == undefined){
+    alert('You didn\'t write anything')
+  } else {
+    addMessage(userName, messageContentInput.value)
+  };
 };
 
 loginForm.addEventListener('submit', login);
+addMessageForm.addEventListener('submit', sendMessage);
 
